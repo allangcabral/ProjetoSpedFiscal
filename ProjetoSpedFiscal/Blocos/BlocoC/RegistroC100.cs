717da24cdb1a02618886d69fe128ec39f14a0c2c
allangcabral <allangcabral@gmail.com>
2017-06-01 20:24:35 -0300

    h

--- ProjetoSpedFiscal/Blocos/BlocoC/RegistroC100.cs
+++ ProjetoSpedFiscal/Blocos/BlocoC/RegistroC100.cs
@@ -10,5 +10,27 @@ namespace ProjetoSpedFiscal.Blocos.BlocoC
         public string COD_MOD { get; set; }
         public short COD_SIT { get; set; }
         public string SER { get; set; }
+        public string NUM_DOC { get; set; }
+        public string CHV_NFE { get; set; }
+        public string DT_DOC { get; set; }
+        public string DT_E_S { get; set; }
+        public double VL_DOC { get; set; }
+        public string IND_PGTO { get; set; }
+        public double VL_DESC { get; set; }
+        public double VL_ABAT_NT { get; set; }
+        public double VL_MERC { get; set; }
+        public string IND_FRT { get; set; }
+		public double VL_FRT { get; set; }
+		public double VL_SEG { get; set; }
+		public double VL_OUT_DA { get; set; }
+		public double VL_BC_ICMS { get; set; }
+		public double VL_ICMS { get; set; }
+		public double VL_BC_ICMS_ST { get; set; }
+		public double VL_ICMS_ST { get; set; }
+		public double VL_IPI { get; set; }
+		public double VL_PIS { get; set; }
+		public double VL_COFINS { get; set; }
+		public double VL_PIS_ST { get; set; }
+		public double VL_COFINS_ST { get; set; }
     }
 }
