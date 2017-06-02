c375a59b7fad89ec0b0ff06d63991bd480f3b6ed
allangcabral <allangcabral@gmail.com>
2017-06-02 19:21:06 -0300

    s

---
+++ ProjetoSpedFiscal/Blocos/BlocoC/RegistroC130.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ProjetoSpedFiscal.Blocos.BlocoC
+{
+    public class RegistroC130
+    {
+        public string REG { get; set; }
+        public double VL_SERV_NT { get; set; }
+        public double VL_BC_ISSQN { get; set; }
+        public double VL_ISSQN { get; set; }
+        public double VL_BC_IRRF { get; set; }
+        public double VL_IRRF { get; set; }
+        public double VL_BC_PREV { get; set; }
+        public double VL_PREV { get; set; }
+    }
+}
---
+++ ProjetoSpedFiscal/Blocos/BlocoC/RegistroC140.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ProjetoSpedFiscal.Blocos.BlocoC
+{
+    public class RegistroC140
+    {
+        public string REG { get; set; }
+        public string IND_EMIT { get; set; }
+        public string IND_TIT { get; set; }
+        public string DESC_TIT { get; set; }
+        public string NUM_TIT { get; set; }
+        public short QTD_PARC { get; set; }
+        public double VL_TIT { get; set; }
+    }
+}
