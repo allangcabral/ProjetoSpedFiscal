4abc55f5c08788b72a30e7f47db3bb0ee5a6824e
allangcabral <allangcabral@gmail.com>
2017-06-02 19:07:49 -0300

    s

---
+++ ProjetoSpedFiscal/Blocos/BlocoC/RegistroC116.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ProjetoSpedFiscal.Blocos.BlocoC
+{
+    public class RegistroC116
+    {
+        public string REG { get; set; }
+        public string COD_MOD { get; set; }
+        public string NR_SAT { get; set; }
+        public string CHV_CFE { get; set; }
+        public string NUM_CFE { get; set; }
+        public string DT_DOC { get; set; }
+    }
+}
---
+++ ProjetoSpedFiscal/Blocos/BlocoC/RegistroC120.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ProjetoSpedFiscal.Blocos.BlocoC
+{
+    public class RegistroC120
+    {
+        public string REG { get; set; }
+        public string COD_DOC_IMP { get; set; }
+        public string NUM_DOC_IMP { get; set; }
+        public double PIS_IMP { get; set; }
+        public double COFINS_IMP { get; set; }
+        public string NUM_ACDRAW { get; set; }
+    }
+}
