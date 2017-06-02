918b35c2645a3b5487b0a240eaf78cbb85679af8
allangcabral <allangcabral@gmail.com>
2017-06-02 18:25:12 -0300

    a

---
+++ ProjetoSpedFiscal/Blocos/BlocoC/RegistroC112.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ProjetoSpedFiscal.Blocos.BlocoC
+{
+    public class RegistroC112
+    {
+        public string REG { get; set; }
+        public string COD_DA { get; set; }
+        public string UF { get; set; }
+        public string NUM_DA { get; set; }
+        public string COD_AUT { get; set; }
+        public double VL_DA { get; set; }
+        public string DT_VCTO { get; set; }
+        public string DT_PGTO { get; set; }
+    }
+}
