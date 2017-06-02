5cf8e50fe00b83c67f11179624d17811288a6b90
allangcabral <allangcabral@gmail.com>
2017-06-02 19:38:54 -0300

    x

---
+++ ProjetoSpedFiscal/Blocos/BlocoC/RegistroC165.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ProjetoSpedFiscal.Blocos.BlocoC
+{
+    public class RegistroC165
+    {
+        public string REG { get; set; }
+        public string COD_PART { get; set; }
+        public string VEIC_ID { get; set; }
+        public string COD_AUT { get; set; }
+        public string NR_PASSE { get; set; }
+        public string HORA { get; set; }
+        public double TEMPER { get; set; }
+        public string QTD_VOL { get; set; }
+        public double PESO_BRT { get; set; }
+        public double PESO_LIQ { get; set; }
+        public string NOM_MOT { get; set; }
+        public string CPF { get; set; }
+        public string UF_ID { get; set; }
+    }
+}
