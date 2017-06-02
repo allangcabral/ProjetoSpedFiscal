d79026482b4b3e577780d02e4a1f13412441f4b0
allangcabral <allangcabral@gmail.com>
2017-06-02 20:54:56 -0300

    h

---
+++ ProjetoSpedFiscal/Blocos/BlocoC/RegistroC170.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ProjetoSpedFiscal.Blocos.BlocoC
+{
+    public class RegistroC170
+    {
+        public string REG { get; set; }
+        public short NUM_ITEM { get; set; }
+        public string COD_ITEM { get; set; }
+        public string DESCR_COMPL { get; set; }
+
+    }
+}
