58ac74c95164cf86396ea87357407ffab89a2797
allangcabral <allangcabral@gmail.com>
2017-06-03 15:35:33 -0300

    h

---
+++ ProjetoSpedFiscal/Blocos/BlocoC/RegistroC174.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ProjetoSpedFiscal.Blocos.BlocoC
+{
+    public class RegistroC174
+    {
+        public string REG { get; set; }
+        public string IND_ARM { get; set; }
+        public string NUM_ARM { get; set; }
+        public string DESCR_COMPL { get; set; }
+    }
+}
---
+++ ProjetoSpedFiscal/Blocos/BlocoC/RegistroC175.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ProjetoSpedFiscal.Blocos.BlocoC
+{
+    public class RegistroC175
+    {
+        public string REG { get; set; }
+        public string IND_VEIC_OPER { get; set; }
+        public string CNPJ { get; set; }
+        public string UF { get; set; }
+        public string CHASSI_VEIC { get; set; }
+    }
+}
