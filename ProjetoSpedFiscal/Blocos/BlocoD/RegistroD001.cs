76e432d8e0427a4c7e73a042ea9dc4cdf4c8f4d9
allangcabral <allangcabral@gmail.com>
2017-06-04 19:45:42 -0300

    e

---
+++ ProjetoSpedFiscal/Blocos/BlocoD/RegistroD001.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ProjetoSpedFiscal.Blocos.BlocoD
+{
+    public class RegistroD001
+    {
+        public string REG { get; set; }
+        public string IND_MOV { get; set; }
+    }
+}
---
+++ ProjetoSpedFiscal/Blocos/BlocoD/RegistroD100.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ProjetoSpedFiscal.Blocos.BlocoD
+{
+    public class RegistroD100
+    {
+        public string REG { get; set; }
+        public string IND_OPER { get; set; }
+        public string IND_EMIT { get; set; }
+        public string COD_PART { get; set; }
+        public string COD_MOD { get; set; }
+        public string COD_SIT { get; set; }
+        public string SER { get; set; }
+        public string SUB { get; set; }
+        public string NUM_DOC { get; set; }
+        public string CHV_CTE { get; set; }
+        public string DT_DOC { get; set; }
+        public string DT_A_P { get; set; }
+        public string TP_CTe { get; set; }
+        public string CHV_CTE_REF { get; set; }
+        public double VL_DOC { get; set; }
+        public double VL_DESC { get; set; }
+        public string IND_FRT { get; set; }
+        public double VL_SERV { get; set; }
+        public double VL_BC_ICMS { get; set; }
+        public double VL_ICMS { get; set; }
+        public double VL_NT { get; set; }
+        public string COD_INF { get; set; }
+        public string COD_CTA { get; set; }
+    }
+}
