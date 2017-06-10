e4ac3d186984387ff90b551ad7558aec27b37a6e
allangcabral <allangcabral@gmail.com>
2017-06-10 20:05:46 -0300

    a

---
+++ ProjetoSpedFiscal/Blocos/BlocoD/RegistroD101.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ProjetoSpedFiscal.Blocos.BlocoD
+{
+    public class RegistroD101
+    {
+        public string REG { get; set; }
+        public string VL_FCP_UF_DEST { get; set; }
+        public string VL_ICMS_UF_DEST { get; set; }
+        public string VL_ICMS_UF_REM { get; set; }
+    }
+}
---
+++ ProjetoSpedFiscal/Blocos/BlocoD/RegistroD110.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ProjetoSpedFiscal.Blocos.BlocoD
+{
+    public class RegistroD110
+    {
+        public string REG { get; set; }
+        public short NUM_ITEM { get; set; }
+        public string COD_ITEM { get; set; }
+        public double VL_SERV { get; set; }
+        public double VL_OUT { get; set; }
+    }
+}
